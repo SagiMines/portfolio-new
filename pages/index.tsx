8cb49b6ce3107c8d87df3b421340dc65ebf86175
Sagi Mines <sagi1236@gmail.com>
2023-05-08 14:12:36 +0300

    Added the Skills component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
+import Skills from '@/components/Skills';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,18 +16,17 @@ export default function Home() {
 
       <Header />
 
-      {/* Hero */}
       <section id="hero" className="snap-start">
         <Hero />
       </section>
 
-      {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
 
-      {/* Skills */}
-
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
       {/* Projects */}
 
       {/* Contact Me */}
